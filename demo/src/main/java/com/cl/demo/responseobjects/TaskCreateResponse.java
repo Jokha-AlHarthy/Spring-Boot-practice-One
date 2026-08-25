a24ac8c781016fbc53f91d5b6898da7409c93aa9
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-25 14:36:53 +0400

    feat(reponseObjects): Updating the TaskCreateResponse.java file

--- demo/src/main/java/com/cl/demo/responseobjects/TaskCreateResponse.java
+++ demo/src/main/java/com/cl/demo/responseobjects/TaskCreateResponse.java
@@ -1,4 +1,40 @@
 package com.cl.demo.responseobjects;
 
+import com.cl.demo.entities.Task;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class TaskCreateResponse {
+    String taskId;
+    String title;
+    String description;
+    String taskNumber;
+    String taskStatus;
+    String dueDate;
+    Boolean isAssigned;
+
+    public static TaskCreateResponse convert(Task task) {
+        TaskCreateResponse response = new TaskCreateResponse();
+        response.setTaskId(task.getId().toString());
+        response.setTitle(task.getTitle());
+        response.setDescription(task.getDescription());
+        response.setTaskNumber(task.getTaskNumber());
+        response.setTaskStatus(task.getTaskStatus().toString());
+        response.setDueDate(task.getDueDate().toString());
+        response.setIsAssigned(task.getIsAssigned());
+        return response;
+    }
+
+    public static List<TaskCreateResponse> convert(List<Task> taskList) {
+        List<TaskCreateResponse> responseList = new ArrayList<>();
+        for (Task t : taskList) {
+            responseList.add(convert(t));
+        }
+        return responseList;
+    }
 }
