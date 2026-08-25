900e1161ce8b6b1a434be14b034fdfaab864ccac
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-25 16:05:23 +0400

    feat(controllers): Updating the TaskController.java file

--- demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,4 +1,45 @@
 package com.cl.demo.controllers;
 
+import com.cl.demo.requestobjects.TaskCreateRequest;
+import com.cl.demo.requestobjects.TaskUpdateRequest;
+import com.cl.demo.responseobjects.TaskCreateResponse;
+import com.cl.demo.responseobjects.TaskUpdateResponse;
+import com.cl.demo.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("task")
 public class TaskController {
+    @Autowired
+    public TaskService taskService;
+
+    @PostMapping("add")
+    public Map<String, String> addTask(@RequestBody TaskCreateRequest task){
+        return taskService.addTask(task);
+    }
+
+    @GetMapping("getById")
+    public TaskCreateResponse getTaskById(@RequestParam String uuid) {
+        return TaskCreateResponse.convert(taskService.getTaskById(uuid));
+    }
+
+    @GetMapping("getAll")
+    public List<TaskCreateResponse> getAllTasks() {
+
+        return TaskCreateResponse.convert(taskService.getAllTasks());
+    }
+
+    @PutMapping("update")
+    public TaskUpdateResponse updateTask(@RequestBody TaskUpdateRequest updateObj) {
+        return TaskUpdateResponse.convert(taskService.updateTask(updateObj));
+    }
+
+    @DeleteMapping("deleteById")
+    public Boolean deleteTaskById(@RequestParam String id) {
+        return taskService.deleteById(id);
+    }
 }
