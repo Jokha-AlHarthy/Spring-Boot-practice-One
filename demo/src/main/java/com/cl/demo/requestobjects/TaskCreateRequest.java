842a0d031790607322529f44804c46da695ebd75
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-25 11:52:37 +0400

    feat(requestObjects): Adding TaskCreateRequest.java file

---
+++ demo/src/main/java/com/cl/demo/requestobjects/TaskCreateRequest.java
@@ -0,0 +1,18 @@
+package com.cl.demo.requestobjects;
+
+import com.cl.demo.entities.TaskStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class TaskCreateRequest {
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Date startDate;
+    private TaskStatus taskStatus;
+    private boolean isAssigned;
+}
