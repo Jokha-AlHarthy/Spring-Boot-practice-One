9b00ac08df5b0c8451414e67890963a1f0678d90
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-30 08:01:28 +0400

    feat(requestobjects): Updating PhoneNumberUpdateRequest.java file

--- demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
+++ demo/src/main/java/com/cl/demo/requestobjects/PhoneNumberUpdateRequest.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PhoneNumberUpdateRequest {
-
-
+    private String uuid;
+    private String countryCodeToUpdate;
+    private Long phoneNumberToUpdate;
 }
